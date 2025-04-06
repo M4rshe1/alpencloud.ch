3154daa31949af926352fee1ae9cf213e5f1436e
M4rshe1 <colin.heggli@gmail.com>
2025-04-06 17:53:58 +0200

    fix plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,14 @@ export default async function RootLayout({
         <div style={{height: `${BANNERS.length * 36}px`}}/>
         <main
             className="min-h-screen bg-gradient-to-br from-gray-900 via-red-900 to-gray-900 [&:has(>#hero:first-child)]:pt-16 [&:not(:has(>#hero:first-child))]:pt-24 pb-20 [&:not(:has(>#hero:first-child))]:px-4">
-            <PlausibleProvider domain={process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN as string} selfHosted={true} customDomain={process.env.NEXT_PUBLIC_PLAUSIBLE_API_HOST}>
-            {children}
+            <PlausibleProvider
+                domain={process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN || 'alpencloud.ch'}
+                selfHosted={true}
+                trackLocalhost
+                customDomain={process.env.NEXT_PUBLIC_PLAUSIBLE_API_HOST}
+                enabled={process.env.NODE_ENV == 'production'}
+            >
+                {children}
             </PlausibleProvider>
         </main>
         <Footer/>
